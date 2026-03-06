9a6ea901f2524b0b549f603dfafdd4e8a30d852c
jabsina <jabsinapa@gmail.com>
2026-03-07 01:03:05 +0530

    display updated

--- auction/settings.py
+++ auction/settings.py
@@ -125,4 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static')
+]
